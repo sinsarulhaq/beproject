1f0e50404ca1168be841dff00691bd3d8ed7ca07
sinsarul haq vm <sinsarulhaq.pro@gmail.com>
2023-11-06 20:34:28 +0530

    impliment cart functionality

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,11 +1,14 @@
 const { generateToken } = require("../config/jwtToken");
 const User = require("../models/userModel");
+const Product = require("../models/productModel");
+const Cart = require("../models/cartModel");
 const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongodbId");
 const { generateRefreshToken } = require("../config/refreshToken");
 const jwt = require("jsonwebtoken");
 const sendEmail = require("./emailCtrl");
 const crypto = require("crypto");
+const { log } = require("console");
 
 //create a user
 const createUser = asyncHandler( async (req, res) => {
@@ -295,4 +298,55 @@ const getWishlist = asyncHandler(async(req, res) => {
     }
 });
 
-module.exports = { createUser, loginUserCtrl, getallUser, getaUser,  deleteaUser, updateaUSer, blockUser, unblockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken, resetPassword, loginAdminCtrl, getWishlist, saveAddress };
+const userCart = asyncHandler(async(req, res) => {
+    const { cart } = req.body;
+    const { _id } = req.user;
+    validateMongoDbId(_id);
+    
+    try {
+        let products = [];
+        const user = await User.findById(_id);
+        //check if user already have products in cart
+        const alreadyExistCart = await Cart.findOne({orderby: user._id});
+        if(alreadyExistCart){
+            alreadyExistCart.remove()
+        }
+        for(let i= 0; i < cart.length; i++){
+            let object = {};
+            object.product = cart[i]._id
+            object.count = cart[i].count
+            object.color = cart[i].color
+
+            let getPrice = await Product.findById(cart[i]._id).select("price").exec();
+            object.price = getPrice.price;
+            products.push(object);
+        }
+            let cartTotal= 0;
+            for (let i=0; i<products.length; i++){
+                cartTotal = cartTotal + products[i].price * products[i].count;
+
+            }
+            let newCart = await Cart.create({
+                products,
+                cartTotal,
+                orderby: user?._id
+            });
+            res.json(newCart);
+        
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const getUserCart = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    validateMongoDbId(_id);
+    try {
+        const cart = await Cart.findOne({orderby: _id });
+        res.json(cart);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+//8:00 (lets check postman response of getUserCart)
+module.exports = { createUser, loginUserCtrl, getallUser, getaUser,  deleteaUser, updateaUSer, blockUser, unblockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken, resetPassword, loginAdminCtrl, getWishlist, saveAddress, userCart, getUserCart };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updateaUSer, blockUser, unblockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken, resetPassword, loginAdminCtrl, getWishlist, saveAddress } = require("../controller/userCtrl");
+const { createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updateaUSer, blockUser, unblockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken, resetPassword, loginAdminCtrl, getWishlist, saveAddress, userCart, getUserCart } = require("../controller/userCtrl");
 const { authMiddleWare, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
@@ -7,12 +7,14 @@ router.post('/register', createUser);
 
 router.post('/login', loginUserCtrl);
 router.post('/admin-login', loginAdminCtrl);
+router.post('/cart', authMiddleWare, userCart);
 router.post('/forgot-password-token', forgotPasswordToken);
 router.post('/reset-password/:token', resetPassword)
 router.get('/all-users', getallUser);
 router.get("/refresh", handleRefreshToken);
 router.get("/logout", logout);
 router.get('/wishlist', authMiddleWare, getWishlist);
+router.get('/wishlist', authMiddleWare, getUserCart);
 router.get('/:id', authMiddleWare, isAdmin, getaUser);
 
 router.delete('/:id', deleteaUser);
